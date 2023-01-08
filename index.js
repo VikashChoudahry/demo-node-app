e1c42cf09a3d1234e1121d8301fec67cfa27aa8d
Vikash Choudhary <servikash@gmail.com>
2023-01-09 03:12:45 +0530

    A minor change to the API reponse

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello form Node.js Server'))
+app.get('/', (req, res) => res.send('Hi All, if you liked the content and has helped to achieve what you are looking for, please do like and and subscribe. Thank you!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
